3f9f3f48e7f2519a071a67d018c1f99720533d23
Ritik Kumar <152053737+ritikkumar-28@users.noreply.github.com>
2026-08-17 10:50:05 +0530

    Time: 7 ms (63.09%), Space: 46.5 MB (83.06%) - LeetHub

---
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -0,0 +1,44 @@
+class Solution {
+
+    public int[][] matrixBlockSum(int[][] mat, int k) {
+
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[][] prefix = new int[m + 1][n + 1];
+        int[][] answer = new int[m][n];
+
+        
+        for (int i = 1; i <= m; i++) {
+
+            for (int j = 1; j <= n; j++) {
+
+                prefix[i][j] =
+                    mat[i - 1][j - 1]
+                    + prefix[i - 1][j]
+                    + prefix[i][j - 1]
+                    - prefix[i - 1][j - 1];
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                int top = Math.max(0, i - k);
+                int bottom = Math.min(m - 1, i + k);
+
+                int left = Math.max(0, j - k);
+                int right = Math.min(n - 1, j + k);
+
+                answer[i][j] =
+                    prefix[bottom + 1][right + 1]
+                    - prefix[top][right + 1]
+                    - prefix[bottom + 1][left]
+                    + prefix[top][left];
+            }
+        }
+
+        return answer;
+    }
+}
